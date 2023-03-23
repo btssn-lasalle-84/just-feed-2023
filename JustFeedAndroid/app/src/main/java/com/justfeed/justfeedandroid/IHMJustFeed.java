56ec01844c5b113c3772136497a427f1f7ebe027
Async-Mayeul <mayeulfargier10@gmail.com>
2023-03-23 21:36:39 +0100

    JFEED2023-7 création de la classe BaseDeDonnees qui est une interface entre l'application android et une base de données distante

--- JustFeedAndroid/app/src/main/java/com/justfeed/justfeedandroid/IHMJustFeed.java
+++ JustFeedAndroid/app/src/main/java/com/justfeed/justfeedandroid/IHMJustFeed.java
@@ -3,8 +3,13 @@ package com.justfeed.justfeedandroid;
 public class IHMJustFeed {
     private Intervention[] interventions;
     private Distributeur[] distributeurs;
+    private BaseDeDonnees baseDeDonnees;
 
-    public IHMJustFeed(){};
+    public IHMJustFeed(){
+        /**
+         * @todo connexion à une Base de données pour initialiser les distributeurs et interventions.
+         */
+    };
 
     public void listerDistributeurs(){};
     public void visualiserInterventions(){};
