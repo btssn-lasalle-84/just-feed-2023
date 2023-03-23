02c09ad0d548c099d8de215256e422b24194f633
m-salaun <matthieusalaun30@gmail.com>
2023-03-23 18:39:10 +0100

    destructeur, et doxygen

--- JustFeedDesktop/communication.h
+++ JustFeedDesktop/communication.h
@@ -1,3 +1,11 @@
+/**
+ * @file        Communication.h
+ * @brief       Définition de la classe Communication.
+ * @detail      La classe Communication \c Cette classe permet de définir les attributs et les méthodes de la classe.
+ * @author      Salaun Matthieu <matthieusalaun30@gmail.com>
+ * @version     0.1
+ * @date        2023
+*/
 #ifndef COMMUNICATION_H
 #define COMMUNICATION_H
 
@@ -6,6 +14,15 @@ class Communication
 {
 public:
     Communication();
+    /**
+     * @brief Constructeur par défault de la classe Communication
+     * @see Distributeur
+     */
+    ~Communication();
+    /**
+     * @brief Destructeur par défault de la classe Communication
+     * @see Distributeur
+     */
 };
 
 #endif // COMMUNICATION_H
