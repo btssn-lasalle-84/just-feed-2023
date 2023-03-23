5c82f270b1a7366ad816068711e7f11fcfb9715e
Async-Mayeul <mayeulfargier10@gmail.com>
2023-03-23 18:41:34 +0100

    JFEED2023-7 création de la classe Produit

--- JustFeedAndroid/app/src/main/java/com/justfeed/justfeedandroid/Produit.java
+++ JustFeedAndroid/app/src/main/java/com/justfeed/justfeedandroid/Produit.java
@@ -1,4 +1,71 @@
 package com.justfeed.justfeedandroid;
 
+/**
+ * @brief Définition de la Classe Produit.
+ * @details La classe Produit décrit un type de produit, avec ses propriétés : nom, poids, prix.
+ * @author Fargier Mayeul
+ * @version 0.1
+ */
 public class Produit {
+    private int prix; //!< prix du produit
+    private int poidsDuProduit; //!< poids à l'unité du produit
+
+    private String nom; //!< nom du produit
+
+    /**
+     * @brief Constructeur par défaut de la classe Produit.
+     * @see Produit()
+     */
+    public Produit(){
+        this.prix = 0;
+        this.poidsDuProduit = 0;
+    }
+
+    /**
+     * @brief Constructeur d'initialisation de la classe Produit.
+     * @see Produit(int prix, int poidsDuProduit, String nom)
+     * @param prix le prix du produit.
+     * @param poidsDuProduit le poid du produit à l'unité.
+     * @param nom le nom du produit.
+     */
+    public Produit(int prix, int poidsDuProduit, String nom){
+        this.prix = prix;
+        this.poidsDuProduit = poidsDuProduit;
+
+        this.nom = nom;
+    }
+
+    //Accesseurs
+    /**
+     * @brief Accesseur du prix du produit.
+     * @return prix le prix du produit
+     */
+    public int getPrix(){
+        return this.prix;
+    }
+
+    /**
+     * @brief Accesseur du poids du produit.
+     * @return poidsDuProduit le poids du produit à l'unité
+     */
+    public int getPoidsDuProduit(){
+        return this.poidsDuProduit;
+    }
+
+    /**
+     * @brief Accesseur du nom du produit.
+     * @return nom le nom du produit
+     */
+    public String getNom(){
+        return this.nom;
+    }
+
+    //Mutateur
+    /**
+     * @brief Mutateur du prix du produit.
+     * @param nouveauPrix le nouveau prix du produit
+     */
+    public void modifierPrix(int nouveauPrix){
+        this.prix = nouveauPrix;
+    }
 }
