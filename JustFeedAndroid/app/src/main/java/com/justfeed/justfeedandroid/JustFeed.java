764374ca1f9fd507836135a8e5f250690ddfd6f5
tvaira <tvaira@free.fr>
2023-03-24 05:54:35 +0100

    Remanie le code

---
+++ JustFeedAndroid/app/src/main/java/com/justfeed/justfeedandroid/JustFeed.java
@@ -0,0 +1,93 @@
+/**
+ * @file JustFeed.java
+ * @brief Déclaration de l'activité principale JustFeed
+ * @author FARGIER Mayeul
+ */
+
+package com.justfeed.justfeedandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * @class JustFeed
+ * @brief L'activité principale
+ */
+public class JustFeed extends AppCompatActivity
+{
+    /**
+     * Constantes
+     */
+    private static final String TAG = "_JustFeed_";  //!< TAG pour les logs (cf. Logcat)
+
+    /**
+     * Attributs
+     */
+
+    /**
+     * Ressources GUI
+     */
+
+    /**
+     * @brief Méthode appelée à la création de l'activité
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate()");
+    }
+
+    /**
+     * @brief Méthode appelée au démarrage après le onCreate() ou un restart après un onStop()
+     */
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d(TAG, "onStart()");
+    }
+
+    /**
+     * @brief Méthode appelée après onStart() ou après onPause()
+     */
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.d(TAG, "onResume()");
+    }
+
+    /**
+     * @brief Méthode appelée après qu'une boîte de dialogue s'est affichée (on reprend sur un onResume()) ou avant onStop() (activité plus visible)
+     */
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.d(TAG, "onPause()");
+    }
+
+    /**
+     * @brief Méthode appelée lorsque l'activité n'est plus visible
+     */
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.d(TAG, "onStop()");
+    }
+
+    /**
+     * @brief Méthode appelée à la destruction de l'application (après onStop() et détruite par le système Android)
+     */
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy()");
+    }
+}
