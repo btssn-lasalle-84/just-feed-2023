9614bc72143ba89c6b679870af63176b3b113573
tvaira <tvaira@free.fr>
2023-05-16 08:30:45 +0200

    Revue de code

--- JustFeedAndroid/app/src/main/java/com/justfeed/justfeedandroid/Operateur.java
+++ JustFeedAndroid/app/src/main/java/com/justfeed/justfeedandroid/Operateur.java
@@ -1,5 +1,7 @@
 package com.justfeed.justfeedandroid;
 
+import android.util.Log;
+
 /**
  * @brief Définition de la classe Operateur.
  * @details La classe Operateur \c Operateur permet choisir l'opérateur
@@ -10,11 +12,49 @@ package com.justfeed.justfeedandroid;
 public class Operateur
 {
     /**
+     * Constantes
+     */
+    private static final String TAG = "_Operateur"; //!< TAG pour les logs (cf. Logcat)
+
+    /**
      * Attributs
      */
-    private String nom; //!< Nom de l'opérateur
-    private String prenom; //!< Prénom de l'opérateur
-    private int idOperateur; //!< Identifiant de l'opérateur
+    private int    idOperateur; //!< Identifiant de l'opérateur dans la table Operateur
+    private String nom;         //!< Nom de l'opérateur
+    private String prenom;      //!< Prénom de l'opérateur
+    private String identifiant; //!< Identifiant de l'opérateur
+    private String email;       //!< Email de l'opérateur
+
+    /**
+     * @brief Constructeur d'initialisation de la classe Operateur
+     * @param nom nom de l'opérateur
+     * @param prenom prenom de l'opérateur
+     */
+    public Operateur(String nom, String prenom)
+    {
+        Log.d(TAG, "Operateur(" + nom + ", " + prenom + ")");
+        this.idOperateur = -1;
+        this.nom         = nom;
+        this.prenom      = prenom;
+        this.identifiant = prenom.substring(0, 1).toLowerCase() + this.nom.toLowerCase();
+        this.email       = "";
+    }
+
+    /**
+     * @brief Constructeur d'initialisation de la classe Operateur
+     * @param nom nom de l'opérateur
+     * @param prenom prenom de l'opérateur
+     * @param idOperateur identifiant de l'opérateur
+     */
+    public Operateur(String nom, String prenom, int idOperateur)
+    {
+        Log.d(TAG, "Operateur(" + nom + ", " + prenom + " ," + idOperateur + ")");
+        this.idOperateur = idOperateur;
+        this.nom         = nom;
+        this.prenom      = prenom;
+        this.identifiant = prenom.substring(0, 1).toLowerCase() + this.nom.toLowerCase();
+        this.email       = "";
+    }
 
     /**
      * @brief Constructeur d'initialisation de la classe Operateur
@@ -22,11 +62,16 @@ public class Operateur
      * @param prenom , prenom de l'opérateur
      * @param idOperateur , identifiant de l'opérateur
      */
-    public Operateur(String nom, String prenom, int idOperateur)
+    public Operateur(String nom, String prenom, String identifiant, String email, int idOperateur)
     {
+        Log.d(TAG,
+              "Operateur(" + nom + ", " + prenom + " ," + identifiant + " ," + email + " ," +
+                idOperateur + ")");
+        this.idOperateur = idOperateur;
         this.nom         = nom;
         this.prenom      = prenom;
-        this.idOperateur = idOperateur;
+        this.identifiant = identifiant;
+        this.email       = email;
     }
 
     /**
@@ -34,20 +79,60 @@ public class Operateur
      */
 
     /**
+     * @brief Accesseur de l'identifiant numérique de l'opérateur
+     * @return idOperateur
+     */
+    public int getIdOperateur()
+    {
+        return this.idOperateur;
+    }
+
+    /**
      * @brief Accesseur du nom de l'opérateur
      * @return nom
      */
-    public String getNom() { return this.nom; }
+    public String getNom()
+    {
+        return this.nom;
+    }
 
     /**
      * @brief Accesseur du prénom de l'opérateur
      * @return prenom
      */
-    public String getPrenom() { return this.prenom; }
+    public String getPrenom()
+    {
+        return this.prenom;
+    }
 
     /**
-     * @brief Accesseur de l'identifiant numérique de l'opérateur
-     * @return idOperateur
+     * @brief Accesseur de l'identifiant de l'opérateur
+     * @return identifiant
      */
-    public int getIdOperateur() { return this.idOperateur; }
+    public String getIdentifiant()
+    {
+        return this.identifiant;
+    }
+
+    /**
+     * @brief Accesseur de l'email de l'opérateur
+     * @return prenom
+     */
+    public String getEmail()
+    {
+        return this.email;
+    }
+
+    /**
+     * Mutateurs
+     */
+
+    /**
+     * @brief Mutateur de l'identifiant numérique de l'opérateur
+     * @param idOperateur
+     */
+    public void setIdOperateur(int idOperateur)
+    {
+        this.idOperateur = idOperateur;
+    }
 }
